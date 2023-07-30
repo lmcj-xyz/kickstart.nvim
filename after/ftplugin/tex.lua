649dc8ae5bff1b0ab9937e5d8ccc4de7d2aab160
LMCJ T470 <dev@lmcj.xyz>
2023-07-30 10:29:52 +0100

    Updated tex and cmp for usability
    - tex.lua options for conceal
    - cmp.lua removed the use of tab for autocompletion

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -4,9 +4,12 @@ vim.opt_local.indentexpr = '4'
 vim.opt_local.softtabstop = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
+--vim.opt_local.nosmarttab = 'on'
+--vim.opt_local.noexpandtab = 1
+--vim.opt_local.nopreserveindent = 1
+--vim.opt_local.nocopyindent = 1
 -- Temporary mappings for article and slides creation
 --vim.keymap.set('n', 'c-s', '5Gx11G^VjjjI%^[', { desc = 'Comment and uncomment for slides compilation' })
 --vim.keymap.set('n', 'c-a', '11G^Vjjjx5GI%^[', { desc = 'Comment and uncomment for article compilation' })
 --slides   5Gx11G^VjjjI%^[
 --article  11G^Vjjjx5GI%^[
-
